2c23530ee0408d325a7919ceb18997d15bdef049
shankershawn <42121132+shankershawn@users.noreply.github.com>
2022-03-27 18:52:51 +0530

    Update battery_post.sh

--- battery_status_post/battery_post.sh
+++ battery_status_post/battery_post.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while true
 do
-	export level=acpi | sed "s|%||" | awk {'printf $4'} | sed "s|,||"
-	export status=acpi | sed "s|,||" | awk {'printf $3'} | sed "s|Discharging|1|" | sed "s|Charging|2|"
+	export level=$(acpi | sed "s|%||" | awk {'printf $4'} | sed "s|,||")
+	export status=$(acpi | sed "s|,||" | awk {'printf $3'} | sed "s|Discharging|1|" | sed "s|Charging|2|")
 	curl -X POST https://battery-service-staging.herokuapp.com/v1/batterylevel/poom/tzOsQ7es/${status}/${level}
 	sleep 5
 done
