4071406d789c8ec1b29e50222c52035ccac37894
Shankarsan Ganai <shankershawn@gmail.com>
2022-06-05 00:21:53 +0530

    Added broker id and removed commented code

--- kafka_server/start.sh
+++ kafka_server/start.sh
@@ -3,6 +3,7 @@ echo $KAFKA_ZOOKEEPER_CONNECT
 echo $KAFKA_ADVERTISED_LISTENERS
 sed -i "s|zookeeper.connect=localhost:2181|zookeeper.connect=$KAFKA_ZOOKEEPER_CONNECT|g" config/server.properties
 sed -i "s|#advertised.listeners=PLAINTEXT://your.host.name:9092|advertised.listeners=$KAFKA_ADVERTISED_LISTENERS|g" config/server.properties
-#sed -i "s|#listeners=PLAINTEXT://:9092|listeners=$KAFKA_ADVERTISED_LISTENERS|g" config/server.properties
+sed -i "s|#listeners=PLAINTEXT://:9092|listeners=PLAINTEXT://:9092|g" config/server.properties
+sed -i "s|broker.id=0|broker.id=100|g" config/server.properties
 #listeners=PLAINTEXT://:9092
 sh bin/kafka-server-start.sh config/server.properties
