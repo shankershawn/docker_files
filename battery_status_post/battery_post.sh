06c24185e3269b3aee33616f59674f0fc258035d
Oracle Public Cloud User <opc@instance-20220205-0113.subnet02050116.vcn02050116.oraclevcn.com>
2022-06-04 18:53:36 +0000

    Updated battery post

--- battery_status_post/battery_post.sh
+++ battery_status_post/battery_post.sh
@@ -2,7 +2,7 @@
 while true
 do
 	export level=$(acpi | sed "s|%||" | awk {'printf $4'} | sed "s|,||")
-	export status=$(acpi | sed "s|,||" | awk {'printf $3'} | sed "s|Discharging|1|" | sed "s|Charging|2|")
+	export status=$(acpi | sed "s|,||" | awk {'printf $3'} | sed "s|Discharging|1|" | sed "s|Charging|2|" | sed "s|Full|2|")
 	curl -X POST https://battery-service-staging.herokuapp.com/v1/batterylevel/poom/tzOsQ7es/${status}/${level}
 	sleep 5
 done
